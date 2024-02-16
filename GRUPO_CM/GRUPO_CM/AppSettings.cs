a425b874b6e2d38f6bb4c16590daede95069cc3d
Ricardo Anaya Perez <riichy.anaya@gmail.com>
2024-02-16 11:17:33 -0600

    Se agrega el proyecto

---
+++ GRUPO_CM/GRUPO_CM/AppSettings.cs
@@ -0,0 +1,28 @@
+﻿using AppNotas.Modelo;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CarritoCompras
+{
+    public class AppSettings
+    {
+        public static string ApiFirebase = "https://test-2c871-default-rtdb.firebaseio.com/";
+        private static string KeyAplication = "AIzaSyChU2ATqKVe9cSlkpgFl7QR9Fe10xYF_5I";
+
+
+        public static ResponseAuthentication oAuthentication { get; set; }
+        private static string ApiUrlGoogleApis = "https://identitytoolkit.googleapis.com/v1/";
+
+        public static string ApiAuthentication(string tipo)
+        {
+            if (tipo == "LOGIN")
+                return ApiUrlGoogleApis + "accounts:signInWithPassword?key=" + KeyAplication;
+            else if (tipo == "SIGNIN")
+                return ApiUrlGoogleApis + "accounts:signUp?key=" + KeyAplication;
+            else
+                return "";
+        }
+
+    }
+}
